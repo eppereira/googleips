8ea0c546d693f1dff2a0cd04bae9f369a777593a
Eder Pereira <ede@megasolucoesti.com>
2023-05-09 19:34:03 -0300

    Add IP Addresses from GCP

--- extract.py
+++ extract.py
@@ -1,17 +1,14 @@
 import json, urllib3
-
-
-
-urls = ["https://www.gstatic.com/ipranges/goog.json"]
+ 
+urls = ["https://www.gstatic.com/ipranges/goog.json","https://www.gstatic.com/ipranges/cloud.json"]
 arquivo = open('enderecos', 'a')
 for u in urls:
-    print(u)
     response = urllib3.request("GET", u)
     dados = json.loads(response.data.decode('utf-8'))
     prefixos = dados["prefixes"]
 
     for p in prefixos:
-        arquivo.write(p["ipv4Prefix"]+'\n')
-        # print(p["ipv4Prefix"])
+        if "ipv4Prefix" in p:
+            arquivo.write(p["ipv4Prefix"]+'\n')
 
 arquivo.close
